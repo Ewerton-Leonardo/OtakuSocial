476c7f5b60baf72e459498af2a019c913ccb6efb
Ewerton-Leonardo <33091155+Ewerton-Leonardo@users.noreply.github.com>
2017-12-13 01:43:15 -0200

    Delete publicacao.py

--- publicacao.py
+++
@@ -1,9 +0,0 @@
-class Publicacao:
-    def __init__(self, email_user, texto, local='', email_amigo=''):
-        self.email_user=email_user
-        self.texto=texto
-        self.local=local
-        self.comentarios=list()
-        self.marcar_amigo=email_amigo
-        self.publi=['Texto: ' + str(texto), 'Local: ' + str(local), 'Amigo: ' + str(email_amigo)]
-        self.dados_publi=dict()
